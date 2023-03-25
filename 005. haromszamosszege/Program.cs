6bb557bdc28b445b5bb2806d301563e22a467801
Dömötör Gyula <grattlesnake@gmail.com>
2023-03-25 21:05:17 +0100

    Hát három szám összege :D

---
+++ 005. haromszamosszege/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _005.haromszamosszege
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kérek egy számot!");
+            int szam1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Kérek még egy számot!");
+            int szam2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Kérek egy harmadik számot!");
+            int szam3 = Convert.ToInt32(Console.ReadLine());
+            int osszeg = szam1 + szam2 + szam3;
+            Console.WriteLine("A három szám összege: "  +osszeg + "." + "\n");
+
+            Console.ReadKey();
+        }
+    }
+}
